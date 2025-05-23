111051a90b769e316c5d26fac51e996465023a55
quanzz123 <nguyenquandc37@gmail.com>
2025-05-23 15:23:43 +0700

    file manager

---
+++ FIXHUB/Areas/Admin/Controllers/TechnicianController.cs
@@ -0,0 +1,19 @@
+﻿using FIXHUB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIXHUB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TechnicianController : Controller
+    {
+        private readonly FixHubDbContext _context;
+        public TechnicianController(FixHubDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
